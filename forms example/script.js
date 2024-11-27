b9a6b96272e92270956d5fbae78cfd7a0095abe3
Gregory271 <gregory.rivera11@gmail.com>
2024-11-27 15:08:50 -0500

    streamlined code and added comments

--- forms example/script.js
+++ forms example/script.js
@@ -1,15 +1,18 @@
+//identifies the input box, submit button and text heading at top of website
 const userNameInput = document.getElementById('username');
 const subButton = document.getElementById('submit');
 const titleText = document.getElementById('title-text');
+
+//creates variable that will store users username, we will "check" this value later 
 let userName;
 
-subButton.onclick=function(){
+subButton.onclick=function(){ //event handler that causes code between brackets to execute once subButton has been clicked
   userName = userNameInput.value;
 
-  if(titleText.value == "" || titleText.value == " "){
-    titleText.innerHTML = "Sorry that didnt work, make sure the username field isnt blank. " + userName;
+  if(userName == "" || userName == " "){ //conditional statment that checks if userName field was empty
+    titleText.innerHTML = "Sorry that didnt work, make sure the username field isnt blank. ";
   }
-  else{
+  else{ //if the userName field wasnt empty, this "else statement" acts as a fallback option, a default result for users who enter it correctly
     titleText.innerHTML = "Thank you for your submission " + userName;
   }
 
